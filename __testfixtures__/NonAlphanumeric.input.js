7e38a4aa9d146ea9cd0a71ae06c6bdee26d136c0
Andres Dominguez <andres.dominguez.p@gmail.com>
2017-01-19 22:36:50 -0500

    Add test for param starting with ! and $

---
+++ __testfixtures__/NonAlphanumeric.input.js
@@ -0,0 +1,11 @@
+class SomeStuff {
+  /**
+   * @param {!NavigationService} aaaNavigation
+   * @param {!angular.$route} $route
+   * @param {!angular.Scope} $scope
+   * @param {!angular.$location} $location
+   * @ngInject
+   */
+  constructor(aaaNavigation, $route, $scope, $location) {
+  }
+}
---
+++ __testfixtures__/NonAlphanumeric.output.js
@@ -0,0 +1,11 @@
+class SomeStuff {
+  /**
+   * @param {!angular.$location} $location
+   * @param {!angular.$route} $route
+   * @param {!angular.Scope} $scope
+   * @param {!NavigationService} aaaNavigation
+   * @ngInject
+   */
+  constructor($location, $route, $scope, aaaNavigation) {
+  }
+}
